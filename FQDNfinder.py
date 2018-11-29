40f2564483aee2612c2d5ce7cac24998225272c2
Konstantin Krastev <grizmin@gmail.com>
2018-11-29 23:48:20 +0200

    improvements and more domains

--- FQDNfinder.py
+++ FQDNfinder.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 from socket import gethostbyname_ex as ghbnex
-import socket
-from multiprocessing.pool import ThreadPool, Queue
+from socket import gaierror as GAIError
+from multiprocessing.pool import ThreadPool
 import argparse
 import logging
 import datetime
@@ -17,13 +17,15 @@ logstreamhandler = logging.StreamHandler()
 logstreamhandler.setFormatter(streamformater)
 logger.addHandler(logstreamhandler)
 
-
 class FQDN(object):
     found = False
-    default_search_domains = ['hec.sap.biz', 'rot.hec.sap.biz', 'ams.hec.sap.biz', 'stl.hec.sap.biz', 'sac.hec.sap.biz',
+    default_search_domains = ['rot.hec.sap.biz', 'ams.hec.sap.biz', 'stl.hec.sap.biz', 'sac.hec.sap.biz',
                               'tyo.hec.sap.biz', 'osa.hec.sap.biz', 'syd.hec.sap.biz', 'mcc.rot.hec.sap.biz',
-                              'rot2.hec.sap.biz', 'syd2.hec.sap.biz', 'mow.hec.sap.biz', 'mow2.hec.sap.biz',
-                              'yyz.hec.sap.biz', 'grizmin.org', 'int.grizmin.org']
+                              'rot2.hec.sap.biz', 'irl.hec.sap.biz', 'us1.hec.sap.biz', 'syd2.hec.sap.biz',
+                              'mow.hec.sap.biz', 'mow2.hec.sap.biz',
+                              'yyz.hec.sap.biz', 'grizmin.org', 'int.grizmin.org', 'syd3.hec.sap.biz',
+                              'us4.hec.sap.biz', 'us3.hec.sap.biz',
+                              'us2.hec.sap.biz']
 
     def __init__(self, shortName, search_domain_list = set()):
         self.search_domain_list = search_domain_list
@@ -34,20 +36,28 @@ class FQDN(object):
 
     def get_fqdn(self):
         """
-        return 1st found resolveable FQDN
+        return 1st found resolvable FQDN
 
         :return: triple
         """
+
+        if any([self.shortName.endswith(sn) for sn in self.default_search_domains]):
+            logger.warn('{0} FQDN found in short names list.'.format(self.shortName))
+            self.found = True
+            return ghbnex(self.shortName)
+
         for search_domain in self.search_domain_list:
             possible_fqdn = self.shortName + '.' + search_domain
             try:
-                logger.debug("Processing {0}".format(search_domain))
+                logger.debug("Processing {0}".format(possible_fqdn))
                 hostinfo = ghbnex(possible_fqdn)
                 self.found = True
                 return hostinfo
-            except socket.gaierror:
-                logger.debug(possible_fqdn + 'is not the FQDN')
-                pass
+            except GAIError:
+                logger.debug(possible_fqdn, 'is not the FQDN')
+            finally:
+                if not self.found:
+                    return (None, None, None)
 
     def __repr__(self):
         return self.fqdn[0], self.fqdn[1], ''.join(self.fqdn[2])
@@ -81,14 +91,13 @@ if __name__ == '__main__':
 
         logger.debug("Starting {0} on {1} with arguments: {2}".format(sys.argv[0], datetime.datetime.today(), vars(arg)))
 
-        pool = ThreadPool(processes=20)
+        pool = ThreadPool(processes=10)
         multiple_results = pool.map(FQDN, arg.shortName)
         for sn in multiple_results:
             if sn.found:
                 if not arg.long:
                     print(sn.fqdn[0])
                 else:
-                    # print(sn.fqdn[0], ' '.join(sn.fqdn[2]))
                     print(sn)
             else:
                 if not arg.warn:
